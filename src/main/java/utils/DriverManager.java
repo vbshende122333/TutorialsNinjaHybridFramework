fc22e01a81fc9f0aa64ad1497fa904ab0987bf2e
Vishal <vbshende122333@gmail.com>
2025-10-21 16:05:10 +0530

    Updated scripts 3:38

--- src/main/java/utils/DriverManager.java
+++ src/main/java/utils/DriverManager.java
@@ -2,47 +2,65 @@ package utils;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.edge.EdgeOptions;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.FirefoxOptions;
 
 import java.time.Duration;
 
 public class DriverManager {
 
-    private static WebDriver driver;
+    private static final ThreadLocal<WebDriver> driver =new ThreadLocal<>();
     public static void initializeDriver(String browser){
 
-        if(browser == null){
-            driver = new ChromeDriver();
+        if (browser == null || browser.isEmpty()) {
+            browser = "chrome"; // default browser
         }
 
         switch (browser.toLowerCase()){
             case "chrome":
-                driver = new ChromeDriver();
+                ChromeOptions chromeOptions = new ChromeOptions();
+                chromeOptions.addArguments("--headless=new");
+                chromeOptions.addArguments("--no-sandbox");
+                chromeOptions.addArguments("--disable-dev-shm-usage");
+                chromeOptions.addArguments("--disable-gpu");
+                chromeOptions.addArguments("--window-size=1920,1080");
+                driver.set(new ChromeDriver(chromeOptions));
                 break;
 
             case "edge":
-                driver = new EdgeDriver();
+                EdgeOptions edgeOptions = new EdgeOptions();
+                edgeOptions.addArguments("--headless=new");
+                edgeOptions.addArguments("--no-sandbox");
+                edgeOptions.addArguments("--disable-dev-shm-usage");
+                edgeOptions.addArguments("--disable-gpu");
+                edgeOptions.addArguments("--window-size=1920,1080");
+                driver.set(new EdgeDriver(edgeOptions));
                 break;
 
             case "firefox":
-                driver = new FirefoxDriver();
+                FirefoxOptions firefoxOptions = new FirefoxOptions();
+                firefoxOptions.addArguments("--headless");
+                firefoxOptions.addArguments("--width=1920");
+                firefoxOptions.addArguments("--height=1080");
+                driver.set(new FirefoxDriver(firefoxOptions));
                 break;
 
             default:
                 throw new IllegalArgumentException("Invalid Browser: " + browser);
 
         }
-        driver.manage().window().maximize();
-        driver.manage().window().maximize();
-        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+        driver.get().manage().window().maximize();
+        driver.get().manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
 
     }
     public static WebDriver getDriver(){
-        return driver;
+        return driver.get();
     }
 
     public static void quitDriver(){
-        driver.quit();
+        driver.get().quit();
     }
 }
